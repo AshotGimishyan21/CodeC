1ac0b16e9ddf7b45cc6e44f2a683380d7396687e
AshotGimishyan21 <gimishyan.aa@phystech.edu>
2020-07-19 23:45:04 +0300

    Hello, GitHub!

---
+++ word_counter.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  char str[100];
+  int counter = 0;
+
+  fgets(str, 100, stdin);
+  fflush(stdin); // clear stdin
+
+  for (int i = 0; str[i] != '\0'; i++)
+  {
+    if (str[i] != ' ' && (str[i+1]== ' ' || str[i+1]=='\n'))
+      counter++;
+  }
+  printf("%i\n", counter);
+  return 0;
+}
