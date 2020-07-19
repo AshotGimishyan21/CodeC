52e4fbe3e09f4438cd3619649ae9b933fad6fc8a
AshotGimishyan21 <gimishyan.aa@phystech.edu>
2020-07-19 23:58:05 +0300

    New commit

---
+++ z_symbol.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+  char c;
+  scanf("%c\n", &c);
+
+  char str[100];
+  fgets(str, 100, stdin);
+  fflush(stdin);
+
+  int flag = 0;
+
+  for (int i = 0; str[i] != '\0'; i++)
+  {
+    if(c==str[i]){
+      flag = 1;
+      printf("%i\n", i);
+      break;
+    }
+  }
+
+  if (flag==0)
+  {
+    printf("-1\n");
+  }
+  return 0;
+}
