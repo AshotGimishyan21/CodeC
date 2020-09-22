7e9910088dc041053afae5e5934bc0ea1b0ef0d9
AshotGimishyan21 <68514762+AshotGimishyan21@users.noreply.github.com>
2020-09-23 01:24:22 +0300

    Update polindrom.c

--- polindrom.c
+++ polindrom.c
@@ -4,36 +4,33 @@
 
 int main()
 {
-  int n;
-  scanf("%d", &n);
+    int n;
+    scanf("%d", &n);
 
-  int arr[n];
-  int count = 0;
-  
-  for (int i = 0; i < n; i++)
-  {
-    scanf("%d", arr + i);
-  }
-  
-  for (int j = 0; j < n/2; j++)
-  {
-    
-    if(arr[j]==arr[n-j-1])
-    {
-      count++;
-      continue;
+    int arr[n];
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", arr + i);
     }
-    
-    else{
-      printf("%s\n", "NO");
-      break;
+
+    for (int j = 0; j < n / 2; j++) {
+
+        if (arr[j] == arr[n - j - 1]) {
+            count++;
+            continue;
+        }
+
+        else {
+            printf("%s\n", "NO");
+            break;
+        }
     }
-  }
-  
-  if (count == n/2)
-  {
-    printf("%s\n", "YES");
-  }
-  
-  return 0;
+
+    if (count == n / 2) {
+        printf("%s\n", "YES");
+    }
+
+    return 0;
 }
+
