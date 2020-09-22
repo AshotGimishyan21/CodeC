382bf8be2dab58061502e30352b1c760b60d9a16
AshotGimishyan21 <68514762+AshotGimishyan21@users.noreply.github.com>
2020-09-23 01:25:09 +0300

    Update this_symbol.c

--- this_symbol.c
+++ this_symbol.c
@@ -2,27 +2,25 @@
 
 int main()
 {
-  char c;
-  scanf("%c\n", &c);
+    char c;
+    scanf("%c\n", &c);
 
-  char str[100];
-  fgets(str, 100, stdin);
-  fflush(stdin);
+    char str[100];
+    fgets(str, 100, stdin);
+    fflush(stdin);
 
-  int flag = 0;
+    int flag = 0;
 
-  for (int i = 0; str[i] != '\0'; i++)
-  {
-    if(c==str[i]){
-      flag = 1;
-      printf("%i\n", i);
-      break;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (c == str[i]) {
+            flag = 1;
+            printf("%i\n", i);
+            break;
+        }
     }
-  }
 
-  if (flag==0)
-  {
-    printf("-1\n");
-  }
-  return 0;
+    if (flag == 0) {
+        printf("-1\n");
+    }
+    return 0;
 }
