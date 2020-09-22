dd8450e2b90265048d535862eaee9386265b17e1
AshotGimishyan21 <68514762+AshotGimishyan21@users.noreply.github.com>
2020-09-23 01:15:05 +0300

    Update caeser.c

--- caeser.c
+++ caeser.c
@@ -1,36 +1,35 @@
 // Шифр Цезаря
 #include <stdio.h>
 #include <cs50.h> // get_string()
-#include  <stdlib.h> // exit()
+#include <stdlib.h> // exit()
 #include <ctype.h> // atoi()
 #include <string.h> // strlen()
 
-int main(int argc, char const * argv[]) {
+int main(int argc, char const* argv[])
+{
 
-  if ( argc != 2 )
-  {
-    printf("%s", "Open the program as: ./name k");
-    printf("\n%s", "Where k is an integer-key.\n");
-    printf("%s\n", "Now you can try again :)");
-    exit(1);
-  }
+    if (argc != 2) {
+        printf("%s", "Open the program as: ./name k");
+        printf("\n%s", "Where k is an integer-key.\n");
+        printf("%s\n", "Now you can try again :)");
+        exit(1);
+    }
 
-  int key = atoi(argv[1]);
-  string s = get_string("Give me a string: ");
+    int key = atoi(argv[1]);
+    string s = get_string("Give me a string: ");
 
-  for (int i = 0, n = strlen(s); i < n; i++) {
+    for (int i = 0, n = strlen(s); i < n; i++) {
 
-    if ( !isalpha(s[i]) )
-      continue;
+        if (!isalpha(s[i]))
+            continue;
 
-    else if ( isupper(s[i]) )
-      s[i] = ( s[i] + key - 'A' ) % 26 + 'A';
-      
-    else if (islower(s[i]))
-      s[i] = ( s[i] + key - 'a' ) % 26 + 'a';
+        else if (isupper(s[i]))
+            s[i] = (s[i] + key - 'A') % 26 + 'A';
 
-  }
+        else if (islower(s[i]))
+            s[i] = (s[i] + key - 'a') % 26 + 'a';
+    }
 
-  printf("Your string after using key=%i: %s\n", key, s);
-  return 0;
+    printf("Your string after using key=%i: %s\n", key, s);
+    return 0;
 }
