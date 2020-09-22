3f649e1f4e3810ae6f53a75f8684a269e465e0af
AshotGimishyan21 <68514762+AshotGimishyan21@users.noreply.github.com>
2020-09-23 01:19:47 +0300

    Update option.c

--- option.c
+++ option.c
@@ -2,45 +2,47 @@
 #include <stdio.h>
 #include <malloc.h>
 
-int * find_diff_of_options(float * prices, int size);
+int* find_diff_of_options(float* prices, int size);
 
-int main(void) {
-  float * prices = NULL;
-  prices = (float * ) malloc(sizeof(float));
+int main(void)
+{
+    float* prices = NULL;
+    prices = (float*)malloc(sizeof(float));
 
-  int i = 0;
-  while (scanf("%f", prices + i) == 1) {
-    prices = (float * ) realloc(prices, (i + 2) * sizeof(float));
-    i++;
-  }
+    int i = 0;
+    while (scanf("%f", prices + i) == 1) {
+        prices = (float*)realloc(prices, (i + 2) * sizeof(float));
+        i++;
+    }
 
-  int * days = find_diff_of_options(prices, i);
+    int* days = find_diff_of_options(prices, i);
 
-  for (int index = 0; index < i; index++) {
-    printf("\nДень: %d \tЦена акции: %d", index, *(days + index));
-  }
+    for (int index = 0; index < i; index++) {
+        printf("\nДень: %d \tЦена акции: %d", index, *(days + index));
+    }
 
-  printf("\n");
-  free(days);
-  free(prices);
-  return 0;
+    printf("\n");
+    free(days);
+    free(prices);
+    return 0;
 }
 
-int * find_diff_of_options(float * prices, int size) {
-  int * new_arr = malloc(sizeof(size));
+int* find_diff_of_options(float* prices, int size)
+{
+    int* new_arr = malloc(sizeof(size));
 
-  for (int i = 0; i < size; i++) {
-    int k = 1;
-    int arr_end = 0;
-    
-    while (i - k >= 0 && arr_end != 1) {
-      if (prices[i - k] <= prices[i])
-        k += 1;
-      else
-        arr_end = 1;
+    for (int i = 0; i < size; i++) {
+        int k = 1;
+        int arr_end = 0;
+
+        while (i - k >= 0 && arr_end != 1) {
+            if (prices[i - k] <= prices[i])
+                k += 1;
+            else
+                arr_end = 1;
+        }
+        new_arr[i] = k;
     }
-    new_arr[i] = k;
-  }
 
-  return new_arr;
+    return new_arr;
 }
